3bdb4b768e40bd95544944c6d8e23877ffdbe93b
LoViera2657 <lvabrandalize@gmail.com>
2026-03-24 14:58:43 -0300

    app.py

--- app.py
+++ app.py
@@ -200,7 +200,4 @@ if check_password():
             # Envia para o Google Sheets
             aba_lancamentos.append_row(nova_linha)
             st.success("Lançamento salvo com sucesso!")
-            
-    st.write("") # Dá um espacinho em branco
-    if st.button("🔄 Atualizar Tabela e Gráficos"):
             st.rerun()
